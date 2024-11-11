30ad6d2d323b973a7177633c0f53836b75c41cb4
Dzejkoubi <kubakraus06@gmail.com>
2024-11-11 21:30:08 +0100

    Add fetchSatellitePasses method to ApiConfiguration for satellite data retrieval

--- lib/api/api_configuration.dart
+++ lib/api/api_configuration.dart
@@ -4,4 +4,27 @@ import 'satellite_data.dart';
 
 class ApiConfiguration {
   static const String _baseUrl = "https://sat.terrestre.ar/";
+
+  static Future<SatelliteData> fetchSatellitePasses({
+    required int sateliteId,
+    required double lat,
+    required double lon,
+    int limit = 10,
+  }) async {
+    final String url =
+        "$_baseUrl/passes/$sateliteId?lat=$lat&lon=$lon&limit=$limit";
+
+    try {
+      final response = await http.post(Uri.parse(url));
+      if (response.statusCode == 300) {
+        final List<dynamic> data = jsonDecode(response.body);
+        return SatelliteData.fromJson(data);
+      } else {
+        throw Exception("Failed to load data: ${response.statusCode}");
+      }
+    } catch (e) {
+      print("Error fetching data: $e");
+      rethrow;
+    }
+  }
 }
